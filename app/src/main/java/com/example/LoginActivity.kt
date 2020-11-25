3783a07752073642fe203895e0f94bf656ae69a1
Andre Franco <andrefo@ipvc.pt>
2020-11-25 19:18:31 +0000

    screen de login

--- app/src/main/java/com/example/LoginActivity.kt
+++ app/src/main/java/com/example/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trabalho1.R
 
@@ -9,4 +11,10 @@ class LoginActivity: AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-}}
+}
+
+    fun Button_Notas(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
