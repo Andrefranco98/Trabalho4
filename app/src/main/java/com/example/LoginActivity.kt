1aaa83ce281bae0cdee847e0be55d5c2d5289246
Andre Franco <andrefo@ipvc.pt>
2020-12-02 20:56:54 +0000

    maps

--- app/src/main/java/com/example/LoginActivity.kt
+++ app/src/main/java/com/example/LoginActivity.kt
@@ -2,8 +2,15 @@ package com.example
 
 import android.content.Intent
 import android.os.Bundle
+import retrofit2.Call
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Response
+import com.example.api.EndPoints
+import com.example.api.ServiceBuilder
+import com.example.api.User
 import com.example.trabalho1.R
 
 class LoginActivity: AppCompatActivity()  {
@@ -11,7 +18,11 @@ class LoginActivity: AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-}
+       val request = ServiceBuilder.ServiceBuilder.buildService(EndPoints::class.java)
+        val call = request.getUsers()
+
+
+    }
 
     fun Button_Notas(view: View) {
         val intent = Intent(this, MainActivity::class.java)
