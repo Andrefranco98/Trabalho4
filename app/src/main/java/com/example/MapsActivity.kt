956ad92de3b9df363d3ab3c329cfa2e0d94e23cd
Andre Franco <andrefo@ipvc.pt>
2020-12-07 15:00:30 +0000

    periodic location

--- app/src/main/java/com/example/MapsActivity.kt
+++ app/src/main/java/com/example/MapsActivity.kt
@@ -5,6 +5,8 @@ import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.example.api.EndPoints
@@ -12,8 +14,7 @@ import com.example.api.OutputPost
 import com.example.api.Problema
 import com.example.api.ServiceBuilder
 import com.example.trabalho1.R
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -31,6 +32,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var problems: List<Problema>
     private lateinit var lastLocation: Location
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest:LocationRequest
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,6 +77,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
 
         })
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+                lastLocation = p0.lastLocation
+                var loc = LatLng(lastLocation.latitude, lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 15.0f))
+                findViewById<TextView>(R.id.txtcoordenadas).text = "Lat: " + loc.latitude + " - Long: " + loc.longitude
+                Log.d("**** Andre", "new location received - " + loc.latitude + " -" + loc.longitude)
+            }
+        }
+        createLocationRequest()
+
+
     }
 
 
@@ -123,4 +140,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
     }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+// interval specifies the rate at which your app will like to receive updates.
+        locationRequest.interval = 10000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+        Log.d("**** Andre", "onPause - removeLocationUpdates")
+    }
+    public override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+        Log.d("**** Andre", "onResume - startLocationUpdates")
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null /* Looper */)
+    }
+
+
+
+
+
 }
