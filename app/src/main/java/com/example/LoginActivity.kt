cacce938e31d44078d53b801d4250cf54f927b14
Andre Franco <andrefo@ipvc.pt>
2020-12-07 14:10:39 +0000

    shared working

--- app/src/main/java/com/example/LoginActivity.kt
+++ app/src/main/java/com/example/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import retrofit2.Call
@@ -47,10 +48,20 @@ class LoginActivity: AppCompatActivity()  {
                     if (response.body()?.error == false) {
                         val intent = Intent(this@LoginActivity, MapsActivity::class.java)
                         Toast.makeText(this@LoginActivity, "Login efectuado", Toast.LENGTH_SHORT).show()
+
+                        /// GET NAME SHARED PREFERENCES ////
+
+                        var token = getSharedPreferences("username", Context.MODE_PRIVATE)
+                        var editor = token.edit()
+                        editor.putString("username_login_atual",username)
+                        editor.commit()
+
                         startActivity(intent)
                     }else{
                         val c: OutputPost = response.body()!!
                         Toast.makeText(this@LoginActivity, "Login falhou, credenciais erradas.", Toast.LENGTH_SHORT).show()
+
+
 
                     }
 
@@ -60,6 +71,21 @@ class LoginActivity: AppCompatActivity()  {
                 Toast.makeText(this@LoginActivity, "${t.message}", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        var token = getSharedPreferences("username", Context.MODE_PRIVATE)      // declaras o token -> username
+        if(token.getString("username_login_atual"," ") != " ") {            //
+
+
+            val intent = Intent(this@LoginActivity, MapsActivity::class.java)       // ENTRA NA ATIVIDADE
+
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+            startActivity(intent)
+        }
     }
 
 }
